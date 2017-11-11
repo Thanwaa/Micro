64e61293a81d3d778bf8303aa1049f33e547cf76
Thanwaa <33487033+Thanwaa@users.noreply.github.com>
2017-11-11 23:10:51 +0700

    Add files via upload

---
+++ Jonotify.ino
@@ -0,0 +1,122 @@
+void Line_Notify(String message) ;
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#define b 08
+
+ 
+const char* ssid = "Teach";
+const char* password = "30122540";
+
+void Line_Notify(String message) ;
+#define LINE_TOKEN "R8cL4RerwvT9VhPn8K2hCN0XLhZfUcluXZAgtP3qT7M" 
+SoftwareSerial NodeSerial(D2, D3);
+String message = "%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B8%A3%E0%B8%A2%E0%B8%B2%E0%B8%99%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%9B%E0%B8%A5%E0%B8%AD%E0%B8%94%E0%B8%A0%E0%B8%B1%E0%B8%A2";
+void setup () {
+ 
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(b, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void loop() {
+  while (NodeSerial.available() > 0) {
+float val = NodeSerial.parseFloat();
+if (NodeSerial.read() == '\n') {
+if (val == 10) {
+Line_Notify(message);
+} 
+Serial.println(val);
+}
+}
+delay(100);
+
+  Serial.println();
+  HTTPClient http;
+  http.begin("https://projectteach.herokuapp.com/text.txt", "08:3B:71:72:02:43:6E:CA:ED:42:86:93:BA:7E:DF:81:C4:BC:62:30");
+  int httpCode = http.GET();
+  if (httpCode == 200) {
+    String content = http.getString();
+    Serial.println(content);
+
+  int Value3012 = -1;
+  int ValueError = -1;
+  
+  ValueError = content.indexOf("error");
+  Value3012 = content.indexOf("3012");
+
+  if (ValueError > 0) {
+  delay(500);
+  }
+  else {
+    Serial.print("");
+  
+
+        if (Value3012  == -1) 
+    {
+      delay(500);
+      Serial.print("\n 3012\n");
+    }
+  
+    
+        if (Value3012 != -1) 
+    {
+        digitalWrite(b , 1);
+        delay(500);
+        Serial.print("\n 3012\n");
+        
+      }
+      
+     
+      Line_Notify(message);
+  }
+  }else {
+    Serial.println("Fail. error code " + String(httpCode));
+  }
+    delay(1000);
+}
+
+
+void Line_Notify(String message) {
+  WiFiClientSecure client;
+
+  if (!client.connect("notify-api.line.me", 443)) {
+    Serial.println("connection failed");
+    return;   
+  }
+
+  String req = "";
+  req += "POST /api/notify HTTP/1.1\r\n";
+  req += "Host: notify-api.line.me\r\n";
+  req += "Authorization: Bearer " + String(LINE_TOKEN) + "\r\n";
+  req += "Cache-Control: no-cache\r\n";
+  req += "User-Agent: ESP8266\r\n";
+  req += "Content-Type: application/x-www-form-urlencoded\r\n";
+  req += "Content-Length: " + String(String("message=" + message).length()) + "\r\n";
+  req += "\r\n";
+  req += "message=" + message;
+  // Serial.println(req);
+  client.print(req);
+    
+  delay(1000);
+
+  // Serial.println("-------------");
+  while(client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      break;
+    }
+    //Serial.println(line);
+  }
+  // Serial.println("-------------");
+}
---
+++ _5678.ino
@@ -0,0 +1,170 @@
+#include <Password.h>
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+Password password = Password( "3012" ); // Set the Password
+
+// Set the LCD address to 0x27 for a 20 chars and 4 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+SoftwareSerial ArduinoSerial(3,2);
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; //  columns
+int pir = 11;          
+int Buzzer = A0;         
+int alarmValue = 0;
+int fl = 0, state = 0, state1 = 0, state2 = 0, Alarm_fl = 0; // Flag
+int dir = 8, dir1 = 0;
+int A1 = 1 ;
+String test = "                    ";
+
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {10, 9, 8, 7}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6,5, 4}; //co
+// Create the Keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup()
+{
+  Serial.begin(9600); 
+  lcd.begin();  
+  pinMode(Buzzer, OUTPUT);         // Sets the digital pin as output
+  keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+  // Sets First Screen LCD
+  lcd.print(" Password Lock");
+  lcd.setCursor(0, 1);
+  lcd.print(" password ");
+  lcd.setCursor(0, 2);
+  ArduinoSerial.begin(4800);
+}
+
+pinMode( A1,INPUT);
+
+void loop()
+{
+  keypad.getKey(); 
+  motion_on();
+  Line_on();     
+
+}
+
+ if( A1=='3012')
+ { digitalWrite(buzzer,LOW );
+ delay(100);
+ }
+
+
+void keypadEvent(KeypadEvent eKey) {
+  switch (keypad.getState()) {
+    case PRESSED:
+      Serial.print("Pressed: ");
+      Serial.println(eKey);
+      lcd.setCursor(dir1, 3);
+      lcd.print(test);
+      test = "*";
+      lcd.setCursor(dir, 3);
+      lcd.print(test);
+      dir1 = dir;
+      switch (eKey)
+      {
+        case '*':
+          if (fl == 1)
+          {
+            state2 = 1;
+            lcd.setCursor(0, 3);
+            lcd.print("  ** PassWord Lock **  ");
+            dir  = 8;
+            dir1 = 0;
+            test = "                    ";
+          }
+          break;
+        case '#':
+          state2 = 0;
+          checkPassword(); // Call the function CheckPassword
+          password.reset(); //Reset Key
+          dir  = 8;
+          dir1 = 0;
+          test = "                    ";
+          lcd.setCursor(0, 3);
+          lcd.print(test);
+          break;
+        default: password.append(eKey);
+          dir++;
+      }
+  }
+}
+
+void checkPassword() // Function check the password
+{
+  if (password.evaluate()) {
+    //Add code to run if it works
+    while (state2 == 0)
+    {
+      Serial.println("Success");
+      fl = 1;
+      
+      lcd.setCursor(0, 3);
+      lcd.print(" Password UnLock");
+      keypad.getKey();
+      Alarm_off();
+      password.reset();
+    }
+  }
+  else {
+    Serial.println("Eror");
+    //add code to run if it did not work
+    lcd.setCursor(0, 3);
+    lcd.print("   Password Eror ");
+    delay(1000);
+  }
+}
+
+
+void Alarm() // Function Alarm.
+{
+  Serial.print("BUZZER_ON");
+  digitalWrite(Buzzer, HIGH); // Turn on Buzzer
+  delay(100); // delay 100 milliseconds
+  digitalWrite(Buzzer, LOW); // Turn off Buzzer
+  delay(100); // delay 100 milliseconds
+
+}
+
+void Alarm_off() // Function Alarm off.
+{
+  digitalWrite(Buzzer, LOW); //Set buzzer pin is HIGH for stop alarm
+}
+
+void motion_on() // Function Open PIR Motion Sensor.
+{
+  alarmValue = digitalRead(pir); // Read analog sensor 0 or 1.
+  //Serial.println (alarmValue); // Debug Sensor value.
+  if (alarmValue == 1) // Check the value
+  {
+    
+    Alarm();
+    Line_on();
+    Serial.print("on");
+  }
+  else
+  {
+    Alarm_off();
+     Serial.print("OFF");
+  }
+}
+void Line_on()
+{
+  if (alarmValue == 1)
+  {
+  ArduinoSerial.print(10);
+   ArduinoSerial.print("\n");
+    delay(100);
+    Serial.print("LINE\n");
+  }
+}
