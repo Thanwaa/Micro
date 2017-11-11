7198f13523712c63c5ae856df300c45295b994e9
Thanwaa <33487033+Thanwaa@users.noreply.github.com>
2017-11-11 23:11:41 +0700

    Create micro.php

---
+++ micro.php
@@ -0,0 +1,53 @@
+<?php
+
+$strAccessToken = "zt6fbDFA1q1ZANR1pK36VIPplVz/MTWpuGKN0AxW2L1qUjd4AN/dmEtvuaVfE76kdjnzJdqXKHDH/qAp45WaHqTdfOCi4iWts9qGuBvVAkqcEDz5t7wSOfvij6JRlUxhgGCkVzCSEK7N8vzDgzck5QdB04t89/1O/w1cDnyilFU=";
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+$_userId = $arrJson['events'][0]['source']['userId'];
+$_msg = $arrJson['events'][0]['message']['text'];
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$filename = 'text.txt';
+if (file_exists($filename)) {
+$myfile = fopen('text.txt', "w+") or die("Unable to open file!");
+fwrite($myfile, $_msg);
+fclose($myfile);
+} else {
+$myfile = fopen('text.txt', "x+") or die("Unable to open file!");
+fwrite($myfile, $_msg);
+fclose($myfile);
+}
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "สวัสดี ID ของคุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "3012") // เป็นรหัสเปลดล็อด
+{
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "ปลดล็อคเรียบร้อยคับ";
+}else{
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "รหัสไม่ถูกต้อง";
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+
+
+
+?>
